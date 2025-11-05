2c34972767dbdd11c652994576c30c9b9404df12
Michael Lynch <lynchmv@gmail.com>
2025-11-05 13:35:24 -0500

    chore: update m3u source URL

--- get_secret_str.py
+++ get_secret_str.py
@@ -9,7 +9,7 @@ CONFIGURE_URL = f"{HOST}/configure"
 # You should replace these with your actual M3U sources,
 # desired cron schedule, and an optional addon password.
 config_data = {
-    "m3u_sources": ["https://tinyurl.com/drewall8"],
+    "m3u_sources": ["https://raw.githubusercontent.com/BuddyChewChew/buddylive/refs/heads/main/buddylive_v1.m3u"],
     "parser_schedule_crontab": "0 */6 * * *",
     "host_url": HOST,
     "addon_password": "12DevServer3!"
